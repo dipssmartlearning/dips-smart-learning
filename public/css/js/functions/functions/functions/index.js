92ff9bdffc68b6e1e20208ad8cbdd62074fae64e
dipssmartlearning <dipskrishnayearners@gmail.com>
2026-07-22 23:05:55 +0530

    Create index.js

---
+++ public/css/js/functions/functions/functions/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.health = functions.https.onRequest((req, res) => {
+  res.status(200).json({
+    success: true,
+    app: "Dips Smart Learning",
+    message: "Firebase Functions are running successfully."
+  });
+});
