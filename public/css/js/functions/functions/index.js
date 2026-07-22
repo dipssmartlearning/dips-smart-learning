e1a61a4fc366c2d199455357ca8fa760b3156ebe
dipssmartlearning <dipskrishnayearners@gmail.com>
2026-07-22 23:17:40 +0530

    Update index.js

--- public/css/js/functions/functions/index.js
+++ public/css/js/functions/functions/index.js
@@ -1,22 +1,8 @@
-{
-  "name": "functions",
-  "description": "Cloud Functions for Dips Smart Learning",
-  "private": true,
-  "main": "index.js",
-  "engines": {
-    "node": "20"
-  },
-  "scripts": {
-    "serve": "firebase emulators:start --only functions",
-    "shell": "firebase functions:shell",
-    "start": "npm run shell",
-    "deploy": "firebase deploy --only functions",
-    "logs": "firebase functions:log"
-  },
-  "dependencies": {
-    "firebase-admin": "^13.0.2",
-    "firebase-functions": "^6.0.1",
-    "cors": "^2.8.5",
-    "razorpay": "^2.9.4"
-  }
-}
+
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.createOrder = require("./createOrder").createOrder;
+exports.verifyPayment = require("./verifyPayment").verifyPayment;
+exports.paymentWebhook = require("./paymentWebhook").paymentWebhook;
